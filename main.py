6aebda22fc5ff454db2794ff556c815c828ee0b5
tww178 <tinwai.a.wong@gmail.com>
2026-02-02 23:40:11 +0000

    CH1L5-L7 argparse, setting roles, adding command line args my attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
+from google.genai import types
 
 
 def main():
@@ -9,11 +11,19 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])] #I don't understsand this line
+
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages, #Why can it be a list?
     )
 
     #The generate_content method returns a GenerateContentResponse object. Print the .text property of the response to see the model's answer.
@@ -24,10 +34,17 @@ def main():
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
+
+    #print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    #print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
+
+    
 
 
 if __name__ == "__main__":
