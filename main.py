7699ef4f7e116c131511da2f42b5976f12599661
tww178 <tinwai.a.wong@gmail.com>
2026-02-02 16:12:09 +0000

    CH1L4: Token Metadata suggested ans

--- main.py
+++ main.py
@@ -9,26 +9,23 @@ def main():
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
+    
     client = genai.Client(api_key=api_key)
-    prompt_content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=prompt_content,
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
+
     #The generate_content method returns a GenerateContentResponse object. Print the .text property of the response to see the model's answer.
     #The GenerateContentResponse object returned by the Gemini API includes a usage_metadata property. The usage_metadata in turn has both:
     #a prompt_token_count property, showing the number of tokens in the prompt that was sent to the model; and
     #a candidates_token_count property, showing the number of tokens in the model's response.
 
-    print(f"User prompt: {prompt_content}")
-    if response.usage_metadata == None:
-       raise RuntimeError("usage_metadata property is None")
-    else:
-        X = response.usage_metadata.prompt_token_count
-        Y = response.usage_metadata.candidates_token_count
-        metadata_string = f"Prompt tokens: {X}\nResponse tokens: {Y}"
-        print(metadata_string)
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
 
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
