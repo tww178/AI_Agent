343d7c0fc76f6e3b41cec8f7ef5cc113d5158c25
tww178 <tinwai.a.wong@gmail.com>
2026-02-02 14:25:30 +0000

    CH1L3: Gemini API

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
+
+
 def main():
-    print("Hello from tww178-ai-agent-python!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
