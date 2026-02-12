2ac712a517c0b50d84b9179492f6ae29840f65bf
tww178 <tinwai.a.wong@gmail.com>
2026-02-12 17:01:25 +0000

    CH4 L1 my initial ans

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 import argparse
@@ -21,35 +22,47 @@ def main():
 
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])] #I don't understsand this line
-
     client = genai.Client(api_key=api_key) #https://googleapis.github.io/python-genai/genai.html#genai.client.Client
-    response = client.models.generate_content( #https://googleapis.github.io/python-genai/genai.html#genai.models.Models, https://googleapis.github.io/python-genai/genai.html#genai.models.Models.generate_content
-        model="gemini-2.5-flash",
-        contents=messages, #Why can it be a list?
-        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0), #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig
-    )
-
-    #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.tools
-    #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.system_instruction
-    #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.temperature
-
-    #The generate_content method returns a GenerateContentResponse object. Print the .text property of the response to see the model's answer.
-    #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentResponse
-    #The GenerateContentResponse object returned by the Gemini API includes a usage_metadata property. The usage_metadata in turn has both:
-    #a prompt_token_count property, showing the number of tokens in the prompt that was sent to the model; and
-    #a candidates_token_count property, showing the number of tokens in the model's response.
-
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini API response appears to be malformed")
-
+    
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")        
+    
+    for _ in range(20):
+        response = client.models.generate_content( #https://googleapis.github.io/python-genai/genai.html#genai.models.Models, https://googleapis.github.io/python-genai/genai.html#genai.models.Models.generate_content
+            model="gemini-2.5-flash",
+            contents=messages, #Why can it be a list?
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0), #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig
+        )
 
-    #print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    #print("Response tokens:", response.usage_metadata.candidates_token_count)
-    if response.function_calls:
+        #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.tools
+        #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.system_instruction
+        #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentConfig.temperature
+
+        #The generate_content method returns a GenerateContentResponse object. Print the .text property of the response to see the model's answer.
+        #https://googleapis.github.io/python-genai/genai.html#genai.types.GenerateContentResponse
+        #The GenerateContentResponse object returned by the Gemini API includes a usage_metadata property. The usage_metadata in turn has both:
+        #a prompt_token_count property, showing the number of tokens in the prompt that was sent to the model; and
+        #a candidates_token_count property, showing the number of tokens in the model's response.
+
+        if not response.usage_metadata:
+            raise RuntimeError("Gemini API response appears to be malformed")
+
+        if response.candidates:
+            for c in response.candidates:
+                messages.append(c.content)
+
+        #print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        #print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+        if not response.function_calls:
+            print("Response:")
+            print(response.text)
+            break
+
+        function_responses = []
+        
         for function_call in response.function_calls:
             #print(f"Calling function: {function_call.name}({function_call.args})")
             function_call_result = call_function(function_call, args.verbose)
@@ -59,13 +72,16 @@ def main():
                 raise Exception("The FunctionResponse object is None.")
             if function_call_result.parts[0].function_response.response == None:
                 raise Exception("Actual function result is None.")
-            function_call_result_list = function_call_result.parts[0]
             if args.verbose:
                 print(f"-> {function_call_result.parts[0].function_response.response}")
+            function_responses.append(function_call_result.parts[0])
+        
+        messages.append(types.Content(role="user", parts=function_responses))
+        if _ == 19: #Help I am quite sure this part is wrong!
+            sys.exit("The maximum number of iterations is reached and the model still hasn't produced a final response")
+        
 
-    else:
-        print("Response:")
-        print(response.text)
+
 
     
 
